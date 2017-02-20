ef43762fa892d7743f6a41dcd1c49021ffb9b04f
dmbelo <david.belo@mclaren.com>
2017-02-19 23:26:02 -0800

    README updates

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 
 ###Histogram of Oriented Gradients (HOG)
 
-1. Explain how (and identify where in your code) you extracted HOG features from the training images.
+####1. Explain how (and identify where in your code) you extracted HOG features from the training images.
+
+The HOG features were extracted from my training images using the `get_hog_features` function in `main.py`. This function calls the `skimage.feature.hog` from the `skikit-image` module.
+
+I've chosen to extract the HOG features from all three channels in an image and concatenate them into a single feature vector as this showed superior classification performance using an SVM classifier as discussed below.
+
+The HOG space for a few sample images from the training set are displayed here:
+
+*INSERT IMAGES*
+
+####2. Explain how you settled on your final choice of HOG parameters.
+
+The parameters for the HOG algorithm live in a larger `FeatureParameters` class defined in `utils.py`. I arrived at the values there by trial and error. The HOG operation is the most time consuming of the vehicle detection pipeline so it's advantageous to keep the number of features as small as possible for this reason. Also the pixel per cell size of (8,8) was chosen to be roughly the size of important features making up a car, i.e. the size of tail lights, windows, etc. In the interest of fewer features I've also reduced the orientation parameter down to 8 from the suggested value of 9 form the class notes with not noticeable degradation in classification accuracy.
+
+####3. Describe how (and identify where in your code) you trained a classifier using your selected HOG features (and color features if you used them).
--- main.py
+++ main.py
@@ -7,189 +7,12 @@ from moviepy.editor import VideoFileClip
 
 from scipy.ndimage.measurements import label as scipy_label
 from skimage.feature import hog
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.externals import joblib
 
 
-class Classifier():
-    def __init__(self, clf, scaler):
-        self.clf = clf
-        self.scaler = scaler
-
-    def predict(self, features):
-        """
-        Wrapper of SVM.predict which includes feature scaling
-        """
-        features_scaled = self.scaler.transform(features.reshape(1, -1))
-        return self.clf.predict(features_scaled)
 
 
-class FeatureParameters():
-    def __init__(self):
-        self.spatial_size = (32, 32)
-        self.hist_bins = 32
-        self.orientations = 8
-        self.pixels_per_cell = 8
-        self.cells_per_block = 2
-
-
-class SearchParameters():
-    def __init__(self):
-        self.window_size = 64
-        self.scale = 1.25
-        self.cell_per_step = 2
-        self.ystart = 380
-        self.ystop = 660
-
-
-def read_image(file):
-    img = cv2.imread(file)
-    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
-def process_image(img):
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2YCR_CB)
-    return img.astype(np.float32)
-
-
-def print_stats(cars, notcars):
-    """
-    Function to print some characteristics of the dataset
-    """
-    print("Number of car samples: {0}".format(len(cars)))
-    print("Number of non car samples: {0}".format(len(notcars)))
-    img = cv2.imread(cars[0])
-    print("Image shape: {0}x{1}".format(img.shape[0], img.shape[1]))
-    print("Image datatype: {}".format(img.dtype))
-
-
-def get_hog_features(img, p, visualize=False, feature_vector=False):
-
-    channels = range(img.shape[2])
-    features = []
-    img_hog = np.zeros_like(img)
-
-    for i, channel in enumerate(channels):
-        out = hog(img[:, :, channel], orientations=p.orientations,
-                  pixels_per_cell=(p.pixels_per_cell, p.pixels_per_cell),
-                  cells_per_block=(p.cells_per_block, p.cells_per_block),
-                  transform_sqrt=True, visualise=visualize,
-                  feature_vector=feature_vector)
-        if visualize:
-            features.append(out[0])
-            img_hog[:, :, i] = out[1]
-        else:
-            features.append(out)
-
-    return features, img_hog
-
-
-def get_color_hist_features(img, p):
-    """
-    Function to compute color histogram features
-    """
-    # Compute the histogram of the color channels separately
-    channel1_hist = np.histogram(img[:, :, 0], bins=p.hist_bins)
-    channel2_hist = np.histogram(img[:, :, 1], bins=p.hist_bins)
-    channel3_hist = np.histogram(img[:, :, 2], bins=p.hist_bins)
-    # Concatenate the histograms into a single feature vector
-    hist_features = np.concatenate((channel1_hist[0],
-                                    channel2_hist[0],
-                                    channel3_hist[0]))
-    return hist_features
-
-
-def get_bin_color_features(img, p):
-    """
-    Function to compute binned color features
-    """
-    return cv2.resize(img, p.spatial_size).ravel()
-
-
-def extract_features(img, p):
-    bin_features = get_bin_color_features(img, p)
-    hist_features = get_color_hist_features(img, p)
-    hog_features = get_hog_features(img, p, feature_vector=True)[0]
-    features = np.concatenate((bin_features, hist_features,
-                               np.ravel(hog_features)))
-    return features
-
-
-def extract_window_features(img_draw, img_window, block_per_window,
-                            hog_features, ypos, xpos, xleft, ytop, heatmap,
-                            p_search, p_features, classifier):
-    hog_feat0 = hog_features[0][ypos:ypos + block_per_window, xpos:xpos +
-                                block_per_window].ravel()
-    hog_feat1 = hog_features[1][ypos:ypos + block_per_window, xpos:xpos +
-                                block_per_window].ravel()
-    hog_feat2 = hog_features[2][ypos:ypos + block_per_window, xpos:xpos +
-                                block_per_window].ravel()
-
-    hog_features = np.hstack((hog_feat0, hog_feat1, hog_feat2))
-    bin_features = get_bin_color_features(img_window, p_features)
-    hist_features = get_color_hist_features(img_window, p_features)
-
-    features = np.hstack((bin_features, hist_features, hog_features))
-    prediction = classifier.predict(features.reshape(1, -1))
-
-    if prediction == 1:
-        xbox_left = np.int(xleft * p_search.scale)
-        ytop_draw = np.int(ytop * p_search.scale)
-        win_draw = np.int(p_search.window_size * p_search.scale)
-        box = [(xbox_left, ytop_draw + p_search.ystart), (xbox_left + win_draw,
-               ytop_draw + win_draw + p_search.ystart)]
-        cv2.rectangle(img_draw, box[0], box[1], (0, 0, 255), 6)
-        add_heat(heatmap, box)
-
-
-def add_heat(heatmap, box):
-    # for box in bbox_list:
-    heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
-    return heatmap
-
-
-def train(cars, notcars, p):
-    features_car = []
-    for car in cars:
-        img = read_image(car)
-        img_processed = process_image(img)
-        features_car.append(extract_features(img_processed, p))
-
-    features_notcar = []
-    for notcar in notcars:
-        img = read_image(notcar)
-        img_processed = process_image(img)  # png
-        features_notcar.append(extract_features(img_processed, p))
-
-    features = np.vstack((features_car, features_notcar))
-    # Fit a per-column scaler
-    scaler = StandardScaler().fit(features)
-    # Apply the scaler to X
-    features_scaled = scaler.transform(features)
-    # Define the labels vector
-    labels = np.hstack((np.ones(len(features_car)),
-                        np.zeros(len(features_notcar))))
-    # Split up data into randomized training and test sets
-    rand_state = np.random.randint(0, 100)
-    out = train_test_split(features_scaled, labels, test_size=0.2,
-                           random_state=rand_state)
-    features_train, features_test, labels_train, labels_test = out
-
-    # Initialize support vector machine object
-    clf = SVC(kernel="linear")
-    # Check the training time for the SVC
-    t = time.time()
-    clf.fit(features_train, labels_train)
-    print('{0:2.2f} seconds to train SVC...'.format(time.time() - t))
-
-    # Accuracy score
-    accuracy = clf.score(features_test, labels_test)
-    print('Test Accuracy of SVC = {0:2.4f}'.format(accuracy))
-
-    return Classifier(clf, scaler)
-
+p_features = FeatureParameters()
+p_search = SearchParameters()
 
 def slide_and_search(img_search, img_draw, hog_features,
                      classifier, p_search, p_features):
@@ -271,7 +94,11 @@ def process_frame(img):
     return img_draw_cars
 
 
-def main(file, classifier, p_features, p_search):
+def main(file):
+    classifier = joblib.load('classifier.pkl')
+    p_features = FeatureParameters()
+    p_search = SearchParameters()
+
     video_extensions = {'.mp4', '.mov'}
     extension = os.path.splitext(file)[1]
     if extension in video_extensions:
@@ -291,19 +118,6 @@ def main(file, classifier, p_features, p_search):
 
 
 if __name__ == "__main__":
-    # Parameters
-    p_features = FeatureParameters()
-    p_search = SearchParameters()
-
-    # import glob
-    # notcars = glob.glob('data/non-vehicles/*/*.png')
-    # cars = glob.glob('data/vehicles/*/*.png')
-    #
-    # print_stats(cars, notcars)
-    # classifier = train(cars, notcars, p_features)
-    # joblib.dump(classifier, 'classifier.pkl')
-
-    classifier = joblib.load('classifier.pkl')
     file = 'short_project_video.mp4'
     # file = 'test_images/test4.jpg'
-    main(file, classifier, p_features, p_search)
+    main(file)
