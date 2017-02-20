029853ec6bed492614e103da23bb721744b7651e
dmbelo <david.belo@mclaren.com>
2017-02-19 23:34:58 -0800

    More remodelling

---
+++ detect_vehicles.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os.path
+
+from scipy.ndimage.measurements import label as scipy_label
+from sklearn.externals import joblib
+from moviepy.editor import VideoFileClip
+from utils import Classifier, FeatureParameters, SearchParameters
+from utils import get_hog_features, slide_and_search, apply_threshold
+from utils import draw_car_boxes, process_image, read_image
+
+
+p_features = FeatureParameters()
+p_search = SearchParameters()
+classifier = joblib.load('classifier.pkl')
+
+
+def pipeline(img):
+    img_draw_search = img.copy()
+    img_draw_cars = img.copy()
+    img_processed = process_image(img)
+    img_search = img_processed[p_search.ystart:p_search.ystop, :, :]
+    shape = img_search.shape
+    img_search = cv2.resize(img_search, (np.int(shape[1] / p_search.scale),
+                                         np.int(shape[0] / p_search.scale)))
+    hog_features = get_hog_features(img_search, p_features)[0]
+    heatmap = slide_and_search(img_search, img_draw_search, hog_features,
+                               classifier, p_search, p_features)
+
+    heatmap_thresh = heatmap.copy()
+    apply_threshold(heatmap_thresh, 2)
+    boxes = scipy_label(heatmap_thresh)
+
+    draw_car_boxes(img_draw_cars, boxes)
+
+    return img_draw_search, img_draw_cars, heatmap
+
+
+def process_frame(img):
+    out = pipeline(img, classifier, p_features, p_search)
+    img_draw_search, img_draw_cars, heatmap = out
+    return img_draw_cars
+
+
+def detect_vehicles():
+    # file = 'short_project_video.mp4'
+    file = 'test_images/test4.jpg'
+    video_extensions = {'.mp4', '.mov'}
+    extension = os.path.splitext(file)[1]
+    if extension in video_extensions:
+        video_output = "project_video_output.mp4"
+        clip = VideoFileClip("short_project_video.mp4")
+        clip = clip.fl_image(process_frame)
+        clip.write_videofile(video_output, audio=False)
+    else:
+        img = read_image(file)
+        out = pipeline(img)
+        img_draw_search, img_draw_cars, heatmap = out
+        plt.subplot(121)
+        plt.imshow(img_draw_cars)
+        plt.subplot(122)
+        plt.imshow(heatmap, cmap='hot')
+        plt.show()
+
+
+if __name__ == "__main__":
+    detect_vehicles()
--- utils.py
+++ utils.py
@@ -139,3 +139,56 @@ def add_heat(heatmap, box):
     # for box in bbox_list:
     heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
     return heatmap
+
+
+def slide_and_search(img_search, img_draw, hog_features,
+                     classifier, p_search, p_features):
+    heatmap = np.zeros_like(img_draw[:, :, 0], np.dtype(np.float32))
+
+    nxblocks = np.int(img_search.shape[1] / p_features.pixels_per_cell) - 1
+    nyblocks = np.int(img_search.shape[0] / p_features.pixels_per_cell) - 1
+    block_per_window = p_search.window_size // p_features.pixels_per_cell - 1
+
+    nxsteps = np.int((nxblocks - block_per_window) / p_search.cell_per_step)
+    nysteps = np.int((nyblocks - block_per_window) / p_search.cell_per_step)
+
+    for xstep in range(nxsteps):
+        for ystep in range(nysteps):
+            ypos = ystep * p_search.cell_per_step
+            xpos = xstep * p_search.cell_per_step
+            xleft = xpos * p_features.pixels_per_cell
+            ytop = ypos * p_features.pixels_per_cell
+            windowed_image = cv2.resize(img_search[ytop:ytop +
+                                        p_search.window_size, xleft:xleft +
+                                        p_search.window_size], (64, 64))
+
+            extract_window_features(img_draw, windowed_image,
+                                    block_per_window, hog_features, ypos, xpos,
+                                    xleft, ytop, heatmap, p_search, p_features,
+                                    classifier)
+
+    return heatmap
+
+
+def apply_threshold(heatmap, threshold):
+    heatmap[heatmap <= threshold] = 0
+    return heatmap
+
+
+def draw_car_boxes(img, boxes):
+    # Iterate through all detected cars
+    bboxes = []
+    for car_number in range(1, boxes[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (boxes[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)),
+                (np.max(nonzerox), np.max(nonzeroy)))
+        bboxes.append(bbox)
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0, 0, 255), 6)
+    # Return the image
+    return bboxes
